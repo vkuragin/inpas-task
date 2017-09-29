564b6193b50de560924bcaefeb25f69c1d8919b3
vk <kaligularnd@gmail.com>
2017-09-29 21:49:26 +0300

    added javadoc

--- src/main/java/vk/dev/inpas/services/CalcService.java
+++ src/main/java/vk/dev/inpas/services/CalcService.java
@@ -6,7 +6,27 @@ import java.util.List;
  * Created by vk on 18.03.2017
  */
 public interface CalcService {
+
+    /**
+     * Calculate water volume by list.
+     *
+     * @param src list of values
+     * @return water volume.
+     */
     Integer calc(List<Integer> src);
+
+    /**
+     * Calculate water volume by string array.
+     *
+     * @param src string array
+     * @return water volume.
+     */
     Integer calc(String src);
+
+    /**
+     * Validate input string.
+     *
+     * @return null if validation succeeded or error message if not.
+     */
     String validate(String input);
 }
